1c7f2179539f00989b0f9831f52a114679d3cab5
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2024-10-30 21:03:12 +0200

    Test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HomeHero from "@/components/HomeHero";
+import PopularMovies from "@/components/PopularMovies";
 
 const Home = () => {
   return (
     <>
       <HomeHero />
+      <PopularMovies />
     </>
   );
 };
---
+++ src/app/popular-movie/[id]/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PopularMovie = () => {
+  return (
+    <>
+      <section className="mt-28 px-20">
+        <h1>PopularMovie</h1>
+      </section>
+    </>
+  );
+};
+
+export default PopularMovie;
